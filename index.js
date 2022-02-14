cb035ccf813d801ebb0548a38bc2ee454140d5be
KimHanByull <asdz9908@naver.com>
2022-02-14 17:35:27 +0900

    cmd를 통한 연습

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World");
+});
+
+
+app.get("/hi", (req, res) => {
+    res.send("Hi, World");
+});
+
+app.post("/", (req, res) => {
+    res.send("POST, World");
+});
+
+
+app.listen(3000);
